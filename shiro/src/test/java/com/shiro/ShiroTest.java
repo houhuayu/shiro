e42a383daadfda818e61640430c2e1287535217c
huayu.hou <huayu.hou@bqjr.cn>
2018-04-20 18:06:48 +0800

    MultiRealm

--- shiro/src/test/java/com/shiro/ShiroTest.java
+++ shiro/src/test/java/com/shiro/ShiroTest.java
@@ -13,6 +13,13 @@ import junit.framework.Assert;
 
 public class ShiroTest {
 	
+	/**
+	 * 
+	* @Description: 常规使用用户名密码登录（配置文件） 
+	* @param     设定文件  
+	* @return void    返回类型  
+	* @throws
+	 */
 	@Test
 	public void testHelloWorld(){
 		Factory<org.apache.shiro.mgt.SecurityManager> factory = 
@@ -32,11 +39,18 @@ public class ShiroTest {
 		subject.logout();
 	}
 	
-	
+	/**
+	 * 
+	* @Title: testMyRealm  
+	* @Description: 常规使用用户名密码登录（Realm) 
+	* @param     设定文件  
+	* @return void    返回类型  
+	* @throws
+	 */
 	@Test
 	public void testMyRealm(){
 		Factory<org.apache.shiro.mgt.SecurityManager> factory = 
-				new IniSecurityManagerFactory("classpath:shiroRealm.ini");
+				new IniSecurityManagerFactory("classpath:shiro-realm.ini");
 		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
 		SecurityUtils.setSecurityManager(securityManager);
 		Subject subject = SecurityUtils.getSubject();
@@ -47,7 +61,24 @@ public class ShiroTest {
 			e.printStackTrace();
 			System.out.println("登录异常");
 		}
-		
+		Assert.assertEquals(true, subject.isAuthenticated());
+		subject.logout();
+	}
+	
+	@Test
+	public void testRealmMulti(){
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = 
+				new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("wang", "123");
+		try {
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			System.out.println("登录异常");
+		}
 		Assert.assertEquals(true, subject.isAuthenticated());
 		subject.logout();
 	}
